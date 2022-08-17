32494eff159ea78787e9d40187aad742163064b7
bigduu <mugeng.du@qq.com>
2022-08-18 00:35:41 +0800

    clippy3.rs

--- clippy/target/debug/deps/clippy1-0e354343703e6a0b.d
+++
@@ -1,7 +0,0 @@
-/Users/bigduu/rustlings/exercises/clippy/target/debug/deps/clippy1-0e354343703e6a0b.rmeta: clippy1.rs
-
-/Users/bigduu/rustlings/exercises/clippy/target/debug/deps/clippy1-0e354343703e6a0b.d: clippy1.rs
-
-clippy1.rs:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
